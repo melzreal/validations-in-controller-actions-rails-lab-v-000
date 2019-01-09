686a7f98619fb0614e5aeb958601323f1398f8e4
Melissa Real <melissa.real@hotmail.com>
2019-01-09 11:34:52 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
+  def new
+  end
+
   def show
   end
 
@@ -9,8 +12,11 @@ class PostsController < ApplicationController
 
   def update
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
