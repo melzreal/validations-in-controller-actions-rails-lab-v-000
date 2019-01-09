09c3509a3f696ba05d3c7e07b9c0e84b5398459b
Melissa Real <melissa.real@hotmail.com>
2019-01-09 14:22:32 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,26 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
-  def new
-  end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
+  def create
+    @post = Post.new(post_params)
+   if @post.valid?
+     @post.save
+     redirect_to post_path(@post)
+   else
+     render :new
+   end
+  end
+
   def update
+    @post = Post.find(params[:id])
     @post.update(post_params)
     if @post.valid?
       redirect_to post_path(@post)
