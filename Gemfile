30b04352f8ab8e35ddaca945ef443df6eb480b45
Melissa Real <melissa.real@hotmail.com>
2019-01-09 14:24:01 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -1,12 +1,14 @@
 source 'https://rubygems.org'
 
-gem 'rails'
+gem 'bundler', '~> 1.16', '>= 1.16.1'
+gem 'rails', '~> 4.2'
 gem 'sqlite3'
 gem 'rspec-rails'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'tux'
 
 group :development, :test do
   gem 'pry'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-resources :posts
-resources :authors
+  get "/authors/new", to: "authors#new", as: "new_author"
+  get "/authors/:id", to: "authors#show", as: "author"
+  post "/authors", to: "authors#create"
+
+  get "/posts/:id", to: "posts#show", as: "post"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  patch "/posts/:id", to: "posts#update"
 end
